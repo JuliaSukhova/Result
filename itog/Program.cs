88cac105683bc1f162363792c91b0450321b2a94
Julia Sukhova <suxova1998@gmail.com>
2023-04-10 08:46:09 +0300

    Добавлены методы

--- itog/Program.cs
+++ itog/Program.cs
@@ -1,25 +1,47 @@
 ﻿// Вводим исходный массив строк с клавиатуры
 
-Console.Write("Введите массив строк через пробел: ");
-string[] arr = Console.ReadLine().Split();
+using System;
+using static System.Console;
 
-// Создаем новый массив и заполняем его строками длиной не более 3 символов
-string[] new_arr = new string[arr.Length];
-int count = 0;
-for (int i = 0; i < arr.Length; i++)
+Clear();
+
+static string[] ReadStringArrayFromConsole()
 {
-    if (arr[i].Length <= 3)
+    Write("Введите массив строк через пробел: ");
+    string[] arr = ReadLine().Split();
+    return arr;
+}
+
+static string[] FilterShortStrings(string[] arr, int maxLength)
+{
+    // Создаем новый массив и заполняем его строками длиной не более maxLength символов
+    string[] new_arr = new string[arr.Length];
+    int count = 0;
+    for (int i = 0; i < arr.Length; i++)
     {
-        new_arr[count] = arr[i];
-        count++;
+        if (arr[i].Length <= maxLength)
+        {
+            new_arr[count] = arr[i];
+            count++;
+        }
     }
+
+    // Создаем новый массив с нужным размером и копируем в него элементы из нового массива
+    string[] final_arr = new string[count];
+    for (int i = 0; i < count; i++)
+    {
+        final_arr[i] = new_arr[i];
+    }
+
+    return final_arr;
 }
 
-// Создаем новый массив с нужным размером и копируем в него элементы из нового массива
-string[] final_arr = new string[count];
-for (int i = 0; i < count; i++)
+static void PrintStringArray(string[] arr)
 {
-    final_arr[i] = new_arr[i];
+    WriteLine("Новый массив строк: " + string.Join(" ", arr));
 }
 
-Console.WriteLine("Новый массив строк: " + string.Join(", ", final_arr)); 
+string[] arr = ReadStringArrayFromConsole();
+int maxLength = 3;
+string[] filteredArr = FilterShortStrings(arr, maxLength);
+PrintStringArray(filteredArr);
